7837ca2f1263aaef2b70cdece405a5f166592cf5
Georgiana B <bere.georgi@gmail.com>
2014-11-07 14:49:30 +0200

    Solution to lifo problem

---
+++ lifo.rb
@@ -0,0 +1,40 @@
+class List
+    def initialize(size=0)
+    	@array= Array.new(size)
+    	@size=size
+     end
+        attr_reader:array 
+        attr_accessor:size 
+
+        def add(item)
+           @size=@size+1
+           @array[size]= item
+        end
+
+        def remove ()  
+        	@array[size]= nil
+            if (size != 0)
+            	@size=@size-1
+            else 
+            	puts " The array is already empty"
+            end
+        end
+
+
+     def print()
+     	puts @array
+     end
+
+end
+
+list= List.new()
+list.add("first")
+list.add(1)
+list.add([3,4,6])
+list.remove()
+list.remove()
+list.remove()
+list.remove()
+list.print()
+
+
